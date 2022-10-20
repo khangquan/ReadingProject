9b3b7f0c0f6391475979eac82a3c36c7861e84e0
KhangQuan <kedoquan@gmail.com>
2022-10-20 21:36:34 +0700

    TEST

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import { LogBox, Platform, StatusBar } from 'react-native'
-import Navigator from './Navigation/Navigator'
 
 import { Provider } from 'react-redux'
 import store from './redux/store/Store'
 
-import DetailScreen from './Screens/DetailScreen/DetailScreen'
 import IntroScreen from './Screens/IntroScreen/IntroScreen'
-import ReadingScreen from './Screens/ReadingScreen/ReadingScreen'
 
 LogBox.ignoreLogs(['Warning: ...']) // Ignore log notification by message
 LogBox.ignoreAllLogs() //Ignore all log notifications
